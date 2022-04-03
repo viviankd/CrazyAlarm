9aa63395b0336da7ed1337e0918b12cd8d94235c
Ming Xia <ming.xia@wsu.edu>
2022-04-03 15:52:00 -0700

    initial UI setup

--- CrazyAlarm/ContentView.swift
+++ CrazyAlarm/ContentView.swift
@@ -8,9 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var Time = "00:00"
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            
+            HStack{
+                Button("+"){
+                }
+                
+                Spacer()
+                
+                Button("Edit"){
+                }
+            }.padding()
+            
+            NavigationView{
+                Form{
+                    Section("Sleep | Wake up"){
+                        Text(Time)
+                            .padding()
+                        Text(Time)
+                            .padding()
+                        Text(Time)
+                            .padding()
+                        Text(Time)
+                            .padding()
+                        Text(Time)
+                            .padding()
+                    }
+                    .navigationTitle("CrazyAlarm")
+                    
+                    Section("Crazy Gaming"){
+                        Text("Game 1")
+                            .padding()
+                        Text("Game 2")
+                            .padding()
+                        Text("Game 3")
+                            .padding()
+                    }
+                }
+            }
+            
+        }
     }
 }
 
