9422d0f9309ed41f002a82a88d061f5abe8e3fb4
Ming Xia <ming.xia@wsu.edu>
2022-04-03 12:13:22 -0700

    Initial Commit

---
+++ CrazyAlarm/CrazyAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrazyAlarmApp.swift
+//  CrazyAlarm
+//
+//  Created by Ming Xia on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CrazyAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
