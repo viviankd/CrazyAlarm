2ba86e3161fac767a973bfaeeeafe6793b507307
Ming Xia <ming.xia@wsu.edu>
2022-04-03 18:37:07 -0700

    basic UI setup

--- CrazyAlarm/ContentView.swift
+++ CrazyAlarm/ContentView.swift
@@ -9,45 +9,109 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var Time = "00:00"
+    @State private var Time = "00:00"
+    @State private var buttonisOn = true
+    @State private var gamebuttonisOn = false
+    @State private var mountAlarm = 1
+
     var body: some View {
         VStack{
-            
-            HStack{
-                Button("+"){
-                }
-                
-                Spacer()
-                
-                Button("Edit"){
-                }
-            }.padding()
-            
             NavigationView{
                 Form{
-                    Section("Sleep | Wake up"){
-                        Text(Time)
-                            .padding()
-                        Text(Time)
-                            .padding()
-                        Text(Time)
-                            .padding()
-                        Text(Time)
-                            .padding()
-                        Text(Time)
-                            .padding()
+                    Section("😴Sleep  |  😃Wake up"){
+                        HStack{
+                            VStack{
+                                Text(Time)
+                                    .font(.largeTitle)
+                                Text("Alarm")
+                            }
+                            Toggle(isOn: $buttonisOn) {
+                            }
+                        }.padding()
                     }
-                    .navigationTitle("CrazyAlarm")
                     
-                    Section("Crazy Gaming"){
-                        Text("Game 1")
-                            .padding()
-                        Text("Game 2")
-                            .padding()
-                        Text("Game 3")
-                            .padding()
+                    Section("💔 Crazy Gaming"){
+                        Toggle("Crazy Music", isOn: $gamebuttonisOn).padding()
+                        Toggle("Crazy Games", isOn: $gamebuttonisOn).padding()
+                        Toggle("Crazy XXXXX", isOn: $gamebuttonisOn).padding()
                     }
                 }
+                .navigationTitle("👻CrazyAlarm")
+                .toolbar{
+                    ToolbarItem(placement: .navigationBarLeading){
+                        Button(action: {
+                            mountAlarm = mountAlarm + 1
+                            print("mount1 = \(mountAlarm)")
+                        }) {
+                            Label("Add", systemImage: "plus")
+                        }
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing){
+                        Button(action: {
+                            mountAlarm = mountAlarm + 1
+                            print("mount2 = \(mountAlarm)")
+                        }) {
+                          Text("Edit")
+                        }
+                    }
+                }
+            }
+            
+            Spacer()
+            
+            HStack{
+                Spacer()
+
+                VStack{
+                    Button(action: {
+                    }) {
+                        Text(Image(systemName: "globe.europe.africa.fill"))
+                            .font(.system(size: 30))
+                    }
+                    Text("World")
+                        .font(.system(size: 15))
+                        .bold()
+                        .italic()
+                }
+                Spacer()
+                VStack{
+                    Button(action: {
+                    }) {
+                        Text(Image(systemName: "alarm"))
+                            .font(.system(size: 30))
+                    }
+                    Text("Alarm")
+                        .font(.system(size: 15))
+                        .bold()
+                        .italic()
+                }
+                Spacer()
+                VStack{
+                    Button(action: {
+                    }) {
+                        Text(Image(systemName: "gamecontroller"))
+                            .font(.system(size: 30))
+                    }
+                    Text("Game")
+                        .font(.system(size: 15))
+                        .bold()
+                        .italic()
+                }
+                Spacer()
+                VStack{
+                    Button(action: {
+                    }) {
+                        Text(Image(systemName: "airplane.departure"))
+                            .font(.system(size: 30))
+
+                    }
+                    Text("Flight")
+                        .font(.system(size: 15))
+                        .bold()
+                        .italic()
+                }
+                Spacer()
+
             }
             
         }
