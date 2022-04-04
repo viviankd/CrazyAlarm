86207d6927fbaea6d16e33dbe0ec2776cb2b1558
Ming Xia <ming.xia@wsu.edu>
2022-04-03 20:30:03 -0700

    background added

--- CrazyAlarm/ContentView.swift
+++ CrazyAlarm/ContentView.swift
@@ -13,107 +13,119 @@ struct ContentView: View {
     @State private var buttonisOn = true
     @State private var gamebuttonisOn = false
     @State private var mountAlarm = 1
-
+    
+    init(){
+        UITableView.appearance().backgroundColor = .clear
+    }
+    
     var body: some View {
-        VStack{
-            NavigationView{
-                Form{
-                    Section("😴Sleep  |  😃Wake up"){
-                        HStack{
-                            VStack{
-                                Text(Time)
-                                    .font(.largeTitle)
-                                Text("Alarm")
+        NavigationView{
+            ZStack{
+                Color.yellow
+                    .opacity(0.7)
+                    .ignoresSafeArea()
+                VStack(alignment: .center){
+                    Spacer()
+                    Form{
+                        Section(header: Text("😴Sleep  |  😃Wake up").font(.headline).foregroundColor(.gray)){
+                            HStack{
+                                VStack{
+                                    Text(Time)
+                                        .font(.largeTitle)
+                                    Text("Alarm")
+                                        .font(.system(size: 15))
+                                }
+                                Toggle(isOn: $buttonisOn) {
+                                }
+                            }.padding()
+                        }
+                        
+                        Section(header: Text("💔 Crazy Alert").font(.headline).foregroundColor(.gray)){
+                            Toggle("Crazy Music", isOn: $gamebuttonisOn).padding()
+                            Toggle("Crazy Games", isOn: $gamebuttonisOn).padding()
+                            Toggle("Crazy XXXXX", isOn: $gamebuttonisOn).padding()
+                        }
+                        .navigationTitle("👻CrazyAlarm")
+                    }
+                    .foregroundColor(Color.black)
+                    .font(.system(size: 18).bold())
+                    Spacer()
+                    .toolbar{
+                        ToolbarItem(placement: .navigationBarLeading){
+                            Button(action: {
+                                mountAlarm = mountAlarm + 1
+                                print("mount1 = \(mountAlarm)")
+                            }) {
+                                Label("Add", systemImage: "plus")
                             }
-                            Toggle(isOn: $buttonisOn) {
+                        }
+                        ToolbarItem(placement: .navigationBarTrailing){
+                            Button(action: {
+                                mountAlarm = mountAlarm + 1
+                                print("mount2 = \(mountAlarm)")
+                            }) {
+                              Text("Edit")
                             }
-                        }.padding()
+                        }
                     }
                     
-                    Section("💔 Crazy Gaming"){
-                        Toggle("Crazy Music", isOn: $gamebuttonisOn).padding()
-                        Toggle("Crazy Games", isOn: $gamebuttonisOn).padding()
-                        Toggle("Crazy XXXXX", isOn: $gamebuttonisOn).padding()
-                    }
-                }
-                .navigationTitle("👻CrazyAlarm")
-                .toolbar{
-                    ToolbarItem(placement: .navigationBarLeading){
-                        Button(action: {
-                            mountAlarm = mountAlarm + 1
-                            print("mount1 = \(mountAlarm)")
-                        }) {
-                            Label("Add", systemImage: "plus")
+                    Spacer()
+                    
+                    HStack(spacing: 15){
+                        Spacer()
+                        VStack(){
+                            Button(action: {
+                            }) {
+                                Text(Image(systemName: "globe.europe.africa.fill"))
+                                    .font(.system(size: 30))
+                            }
+                            Text("World")
+                                .font(.system(size: 15))
+                                .bold()
+                                .italic()
                         }
-                    }
-                    ToolbarItem(placement: .navigationBarTrailing){
-                        Button(action: {
-                            mountAlarm = mountAlarm + 1
-                            print("mount2 = \(mountAlarm)")
-                        }) {
-                          Text("Edit")
+                        Spacer()
+                        VStack{
+                            Button(action: {
+                            }) {
+                                Text(Image(systemName: "alarm"))
+                                    .font(.system(size: 30))
+                            }
+                            Text("Alarm")
+                                .font(.system(size: 15))
+                                .bold()
+                                .italic()
                         }
+                        Spacer()
+                        VStack{
+                            Button(action: {
+                            }) {
+                                Text(Image(systemName: "gamecontroller"))
+                                    .font(.system(size: 30))
+                            }
+                            Text("Game")
+                                .font(.system(size: 15))
+                                .bold()
+                                .italic()
+                        }
+                        Spacer()
+                        VStack{
+                            Button(action: {
+                            }) {
+                                Text(Image(systemName: "airplane.departure"))
+                                    .font(.system(size: 30))
+
+                            }
+                            Text("Flight")
+                                .font(.system(size: 15))
+                                .bold()
+                                .italic()
+                        }
+                        Spacer()
                     }
+                    .background(.yellow)
                 }
             }
-            
-            Spacer()
-            
-            HStack{
-                Spacer()
-
-                VStack{
-                    Button(action: {
-                    }) {
-                        Text(Image(systemName: "globe.europe.africa.fill"))
-                            .font(.system(size: 30))
-                    }
-                    Text("World")
-                        .font(.system(size: 15))
-                        .bold()
-                        .italic()
-                }
-                Spacer()
-                VStack{
-                    Button(action: {
-                    }) {
-                        Text(Image(systemName: "alarm"))
-                            .font(.system(size: 30))
-                    }
-                    Text("Alarm")
-                        .font(.system(size: 15))
-                        .bold()
-                        .italic()
-                }
-                Spacer()
-                VStack{
-                    Button(action: {
-                    }) {
-                        Text(Image(systemName: "gamecontroller"))
-                            .font(.system(size: 30))
-                    }
-                    Text("Game")
-                        .font(.system(size: 15))
-                        .bold()
-                        .italic()
-                }
-                Spacer()
-                VStack{
-                    Button(action: {
-                    }) {
-                        Text(Image(systemName: "airplane.departure"))
-                            .font(.system(size: 30))
-
-                    }
-                    Text("Flight")
-                        .font(.system(size: 15))
-                        .bold()
-                        .italic()
-                }
-                Spacer()
-
-            }
-            
         }
     }
 }
---
+++ CrazyAlarm/FlightTime.swift
@@ -0,0 +1,108 @@
+//
+//  FlightTime.swift
+//  CrazyAlarm
+//
+//  Created by Ming Xia on 4/3/22.
+//
+
+import Foundation
+
+//VStack{
+//    NavigationView{
+//            VStack{
+//                Form{
+//                    Section("😴Sleep  |  😃Wake up"){
+//                        HStack{
+//                            VStack{
+//                                Text(Time)
+//                                    .font(.largeTitle)
+//                                Text("Alarm")
+//                            }
+//                            Toggle(isOn: $buttonisOn) {
+//                            }
+//                        }.padding()
+//                    }
+//                    
+//                    Section("💔 Crazy Gaming"){
+//                        Toggle("Crazy Music", isOn: $gamebuttonisOn).padding()
+//                        Toggle("Crazy Games", isOn: $gamebuttonisOn).padding()
+//                        Toggle("Crazy XXXXX", isOn: $gamebuttonisOn).padding()
+//                    }
+//                }
+//            }
+//            .navigationTitle("👻CrazyAlarm")
+//            .toolbar{
+//                ToolbarItem(placement: .navigationBarLeading){
+//                    Button(action: {
+//                        mountAlarm = mountAlarm + 1
+//                        print("mount1 = \(mountAlarm)")
+//                    }) {
+//                        Label("Add", systemImage: "plus")
+//                    }
+//                }
+//                ToolbarItem(placement: .navigationBarTrailing){
+//                    Button(action: {
+//                        mountAlarm = mountAlarm + 1
+//                        print("mount2 = \(mountAlarm)")
+//                    }) {
+//                      Text("Edit")
+//                    }
+//                }
+//            }
+//    }
+//    Spacer()
+//    HStack{
+//        Spacer()
+//
+//        VStack{
+//            Button(action: {
+//            }) {
+//                Text(Image(systemName: "globe.europe.africa.fill"))
+//                    .font(.system(size: 30))
+//            }
+//            Text("World")
+//                .font(.system(size: 15))
+//                .bold()
+//                .italic()
+//        }
+//        Spacer()
+//        VStack{
+//            Button(action: {
+//            }) {
+//                Text(Image(systemName: "alarm"))
+//                    .font(.system(size: 30))
+//            }
+//            Text("Alarm")
+//                .font(.system(size: 15))
+//                .bold()
+//                .italic()
+//        }
+//        Spacer()
+//        VStack{
+//            Button(action: {
+//            }) {
+//                Text(Image(systemName: "gamecontroller"))
+//                    .font(.system(size: 30))
+//            }
+//            Text("Game")
+//                .font(.system(size: 15))
+//                .bold()
+//                .italic()
+//        }
+//        Spacer()
+//        VStack{
+//            Button(action: {
+//            }) {
+//                Text(Image(systemName: "airplane.departure"))
+//                    .font(.system(size: 30))
+//
+//            }
+//            Text("Flight")
+//                .font(.system(size: 15))
+//                .bold()
+//                .italic()
+//        }
+//        Spacer()
+//    }.background(.yellow)
+//}
+
